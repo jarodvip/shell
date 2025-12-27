fa2ddaea9c787d92cf3211b2eb175508a0f110c9
Jarod <jarod@asd.icu>
2025-12-28 00:31:41 +0800

    修改版本号为100.0.4.2.1

--- tcp.sh
+++ tcp.sh
@@ -4,7 +4,7 @@ export PATH
 #=================================================
 #	System Required: CentOS 7/8,Debian/ubuntu,oraclelinux
 #	Description: BBR+BBRplus+Lotserver
-#	Version: 100.0.4.2
+#	Version: 100.0.4.2.1
 #	Author: 千影,cx9208,YLX
 #	更新内容及反馈:  https://blog.ylx.me/archives/783.html
 #=================================================
